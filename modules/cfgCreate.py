8d34fa2efa3fed5ca770c1a794fbf6d604d58b5d
Andrew <62994051+rarmash@users.noreply.github.com>
2021-06-07 16:01:07 +0300

    Update cfgCreate.py
    Less code.

--- modules/cfgCreate.py
+++ modules/cfgCreate.py
@@ -6,14 +6,10 @@ path = "settings.ini"
 
 def cfgCreate(path):
     config.add_section("BOT")
-    print("Enter bot token: ")
-    config.set("BOT", "token", input())
-    print("Enter bot name: ")
-    config.set("BOT", "name", input())
-    print("Enter ClientID: ")
-    config.set("BOT", "clientid", input())
-    print("Enter bot prefix: ")
-    config.set("BOT", "prefix", input())
+    config.set("BOT", "token", input("Enter bot token: "))
+    config.set("BOT", "name", input("Enter bot name: "))
+    config.set("BOT", "clientid", input("Enter ClientID: "))
+    config.set("BOT", "prefix", input("Enter bot prefix: "))
     
     with open(path, "w+") as config_file:
         path = "settings.ini"
