edfd2022efacc212ea3b1d096082617cfa686dc9
Andrew <62994051+rarmash@users.noreply.github.com>
2021-06-08 15:44:52 +0300

    Adding .bat file

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 from discord.ext import commands
 from modules import cfgCreate
 import os
+from os import system
+from tabulate import tabulate
+
+system("cls")
 
 path = 'settings.ini'
 
@@ -13,7 +17,9 @@ bot = commands.Bot(command_prefix = settings['prefix'])
 
 @bot.event
 async def on_ready():
-    print("Logged in as", bot.user.name)
+    text = "\033[32m {}" .format("Logged in as " + bot.user.name)
+    table = [[text]]
+    print(tabulate(table, tablefmt='grid'), "\033[0m ", sep='\n')
 
 for filename in os.listdir("./cogs"):
     if filename.endswith(".py") and filename != "__init__.py":
---
+++ start.bat
@@ -0,0 +1,6 @@
+pip install discord.py
+pip install youtube_dl
+pip install requests
+pip install discord
+pip install tabulate
+python main.py
