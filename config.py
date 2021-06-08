0bcb0ded88381891c53c4f521c7af0f971e3e715
Andrew <62994051+rarmash@users.noreply.github.com>
2021-06-08 18:55:27 +0300

    Bot nickname delete

--- config.py
+++ config.py
@@ -7,7 +7,6 @@ config.read("settings.ini")
 
 settings = {
     'token': config.get("BOT","token"),
-    'bot': config.get("BOT","name"),
     'id': config.get("BOT","clientid"),
     'prefix': config.get("BOT","prefix")
 }
--- modules/cfgCreate.py
+++ modules/cfgCreate.py
@@ -8,7 +8,6 @@ path = "settings.ini"
 def cfgCreate(path):
     config.add_section("BOT")
     config.set("BOT", "token", input("Enter bot token: "))
-    config.set("BOT", "name", input("Enter bot name: "))
     config.set("BOT", "clientid", input("Enter ClientID: "))
     config.set("BOT", "prefix", input("Enter bot prefix: "))
     
