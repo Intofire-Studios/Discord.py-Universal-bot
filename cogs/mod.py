817d343fca5c804bf6e19fd4a16d7d292cbb7188
Andrew <62994051+rarmash@users.noreply.github.com>
2021-06-14 20:03:16 +0300

    Mod tools
    /kick, /ban, /clear.

---
+++ cogs/mod.py
@@ -0,0 +1,29 @@
+import discord
+from discord.ext import commands
+
+
+class Moderation(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command()
+    @commands.has_permissions(kick_members=True)
+    async def kick(self, ctx, member: discord.Member, *, reason="No reason"):
+        await member.kick(reason=reason)
+        await ctx.send(f"{member.mention} was kicked by {ctx.author.mention}. [{reason}]")
+
+    @commands.command()
+    @commands.has_permissions(ban_members=True)
+    async def ban(self, ctx, member: discord.Member, *, reason="No reason"):
+        await member.ban(reason=reason)
+        await ctx.send(f"{member.mention} was banned by {ctx.author.mention}. [{reason}]")
+
+    @commands.command()
+    @commands.has_permissions(manage_messages=True)
+    async def clear(self, ctx, amount: int):
+        await ctx.channel.purge(limit=amount+1)
+        await ctx.send(f"{amount} messages got deleted.")
+
+
+def setup(bot):
+    bot.add_cog(Moderation(bot))
