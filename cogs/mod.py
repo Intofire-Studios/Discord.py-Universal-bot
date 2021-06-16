9234239d6ce885fd6a7e2cb0179ed43293321cb2
Andrew <62994051+rarmash@users.noreply.github.com>
2021-06-16 22:59:54 +0300

    Added unban

--- cogs/mod.py
+++ cogs/mod.py
@@ -1,5 +1,7 @@
 import discord
+from discord import user
 from discord.ext import commands
+from discord.ext.commands.core import guild_only
 
 
 class Moderation(commands.Cog):
@@ -17,6 +19,19 @@ class Moderation(commands.Cog):
     async def ban(self, ctx, member: discord.Member, *, reason="No reason"):
         await member.ban(reason=reason)
         await ctx.send(f"{member.mention} was banned by {ctx.author.mention}. [{reason}]")
+    
+    @commands.command(description="clear messages", brief="clear [number]") # TODO: Make a description
+    @commands.has_permissions(ban_members=True)
+    async def unban(self, ctx, member):
+        banned_users = await ctx.guild.bans()
+        member_name, member_discriminator = member.split('#')
+
+        for ban_entry in banned_users:
+            user = ban_entry.user
+
+            if (user.name, user.discriminator) == (member_name, member_discriminator):
+                await ctx.guild.unban(user)
+                await ctx.send(f"{user.mention} was unbanned by {ctx.author.mention}")
 
     @commands.command(description="clear messages", brief="clear [number]")
     @commands.has_permissions(manage_messages=True)
