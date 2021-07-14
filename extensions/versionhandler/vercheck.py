a4c6b08f6aac08ca308e6e9629681a5032d0dcdc
Andrew <62994051+rarmash@users.noreply.github.com>
2021-07-15 01:58:11 +0300

    Update autoupdater

--- extensions/versionhandler/vercheck.py
+++ extensions/versionhandler/vercheck.py
@@ -1,5 +1,11 @@
 from os import system
 from time import sleep
+from sys import platform
 def update():
     system("git pull")
     sleep(3)
+    if platform in ["linux", "linux2"]:
+        system("reset")
+    elif platform == "win32":
+        system("cls") 
+    sleep(3) 
